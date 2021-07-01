c0bc4e6dc3e5351e8da0525d9d228c060ef7f4dc
jong-jang <jjh0299@gmail.com>
2021-07-01 18:22:58 +0900

    page

--- js/lot_main.js
+++ js/lot_main.js
@@ -1,5 +1,43 @@
 // lot_main.js
 
+// 이벤트 이미지 데이터
+var evimgDataAll = [
+  {
+    "link" : "http://naver.com",
+    "backi" : "../img/event1.jpg",
+    "name" : "evimg_01",
+  },
+  {
+    "link" : "http://naver.com",
+    "backi" : "../img/event2.png",
+    "name" : "evimg_02",
+  },
+  {
+    "link" : "http://naver.com",
+    "backi" : "../img/event1.jpg",
+    "name" : "evimg_03",
+  },
+  {
+    "link" : "http://naver.com",
+    "backi" : "../img/event2.png",
+    "name" : "evimg_04",
+  },
+  {
+    "link" : "http://naver.com",
+    "backi" : "../img/event1.jpg",
+    "name" : "evimg_05",
+  }
+];
+// 추천 메뉴 데이터
+var bestmenuAll = [
+  {
+    "link" : "http://naver.com",
+    "bestmenuI" : "../img/1.png",
+    "name" : "버거",
+    "price" : "00원",
+    "kcal" : "00kcal"
+  },
+]
 // 인디케이터 li 요소 생성
 var iDc = document.getElementsByClassName('indicator_box')[0];
 var iUl = iDc.getElementsByTagName('ul')[0];
@@ -9,7 +47,7 @@ var evLiFn = function(){
   var insertCode = '<a href="#"><span class="blind"></span></a>';
   evLi.innerHTML = insertCode;
   iUl.appendChild(evLi);
-}
+};
 
 // 이벤트이미지 li 요소 생성
 var evI = document.getElementsByClassName('evimage_box')[0];
@@ -26,39 +64,17 @@ var evimLiFn = function(setData){
   evA.setAttribute('href', setData.link);
   evimLi.setAttribute('class', setData.name);
   evimLi.style.backgroundImage = 'url(' + setData.backi + ')';
+};
 
+// 추천메뉴 이미지 li 요소 생성
+var bmUl = document.getElementsByClassName('bm_box')[0];
 
-}
-// 이벤트 이미지 데이터
-var cardDataAll = [
-  {
-    link : 'http://naver.com',
-    backi : '../img/event1.jpg',
-    name : 'evimg_01',
-  },
-  {
-    link : 'http://naver.com',
-    backi : '../img/event2.png',
-    name : 'evimg_02',
-  },
-  {
-    link : 'http://naver.com',
-    backi : '../img/event1.jpg',
-    name : 'evimg_03',
-  },
-  {
-    link : 'http://naver.com',
-    backi : '../img/event2.png',
-    name : 'evimg_04',
-  },
-  {
-    link : 'http://naver.com',
-    backi : '../img/event1.jpg',
-    name : 'evimg_05',
-  }
-];
+var bmLiFn = function(){
 
-function MakeCard(data){
+};
+
+// 이벤트 이미지함수
+function EvimgCard(data){
   this.link = data.link;
   this.backi = data.backi;
   this.name = data.name;
@@ -71,8 +87,8 @@ for(; i<5; i++){
 
 var i = 0;
 var setCard;
-var cardLen = cardDataAll.length;
-for(; i < cardLen; i++){
-  setCard = new MakeCard(cardDataAll[i]);
+var evimgLen = evimgDataAll.length;
+for(; i < evimgLen; i++){
+  setCard = new EvimgCard(evimgDataAll[i]);
   evimLiFn(setCard);
 };
