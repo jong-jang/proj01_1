afc741b2b4f945def25571bfe4bb0e8ad0a7ff72
jong-jang <jjh0299@gmail.com>
2021-07-21 18:21:32 +0900

    로그인 페이지 수정 및 이벤트 페이지 구축

--- js/lot_sub_event/d_pc.js
+++ js/lot_sub_event/d_pc.js
@@ -9,20 +9,49 @@ $.ajax({
   var myData = data;
 
   // 변수
-var evUl = $('.ev_list');
+var evUl = $('.ev_list'); // html에서 호출
 
-var evLiFn = function(){
+var evLiFn = function(setData){ // 이벤트 리스트 생성
   var evLi = '<li><dl><a href="#"></a><dt></dt><dd></dd></dl></li>';
   evUl.append(evLi);
+
+  evUl.children('li').eq(-1).find('dt').css({backgroundImage : 'url(' + setData.backi + ')'}); // 이미지
+  evUl.children('li').eq(-1).find('dd').text(setData.info); // 설명
 };
 
 var i = 0;
-var myDataLen = myData.length;
+var myDataLen = myData.length; // 데이터의 길이 추출
 
 for(; i<myData.length; i++){
-  evLiFn();
+  evLiFn(myData[i]);
 }
 
 });
+// 당첨자조회 / 진행중인이벤트 클릭 이벤트
+var evC = $('.ev_content');
+var evLi = evC.children('li');
+var evA = evLi.children('a');
+var eL = $('.ev_list');
+var aW = $('.announce_winner');
 
+evLi.eq(0).children('a').on('click', function(e){
+  e.preventDefault();
+
+  var _this = $(this).parent().index();
+
+  evLi.eq(_this).addClass('act');
+  evLi.eq(_this).siblings().removeClass('act');
+  eL.show();
+  aW.hide();
+});
+evLi.eq(1).children('a').on('click', function(e){
+  e.preventDefault();
+
+  var _this = $(this).parent().index();
+
+  evLi.eq(_this).addClass('act');
+  evLi.eq(_this).siblings().removeClass('act');
+  aW.show();
+  eL.hide();
+});
 })(jQuery);
