5e6d617e4bcfb46824787a681bc059c31f066ea1
jong-jang <jjh0299@gmail.com>
2021-07-26 17:00:28 +0900

    브랜드 페이지 수정

--- js/lot_sub_brand/d_pc.js
+++ js/lot_sub_brand/d_pc.js
@@ -5,22 +5,40 @@
 var viewUl = $('.view_list');
 var btnN = $('.next_btn');
 var btnP = $('.prev_btn');
+var indi = $('.indi_box');
+var indiUl = $('.indi_wrap');
+var indiLi = indiUl.children('li');
 
+var permission = 1;
 var n=0;
 btnN.on('click', function(e){
   e.preventDefault();
-  n++;
-  if(n>3){
-    n=3;
+  if(permission){
+    permission = 0;
+    n++;
+    if(n>3){
+      n=3;
+    }
+    indiLi.eq(n).addClass('act');
+    indiLi.eq(n).siblings().removeClass('act');
+    viewUl.animate({marginLeft:-n*100 + '%'}, function(){
+      permission = 1;
+    });
   }
-  viewUl.animate({marginLeft:-n*100 + '%'})
 });
 btnP.on('click', function(e){
   e.preventDefault();
-  n--;
+  if(permission){
+    permission = 0;
+    n--;
   if(n<0){
     n=0;
   }
-  viewUl.animate({marginLeft:-n*100 + '%'})
+  indiLi.eq(n).addClass('act');
+  indiLi.eq(n).siblings().removeClass('act');
+  viewUl.animate({marginLeft:-n*100 + '%'}, function(){
+    permission = 1;
+  });
+  }
 });
 })(jQuery);
